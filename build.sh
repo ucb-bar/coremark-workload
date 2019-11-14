ff7fcf896ef1d5afd3fd26980333f736e182a9b6
abejgonzalez <abe.j.gonza@gmail.com>
2019-11-13 21:55:16 -0800

    add a baremetal workload

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echo "Building Coremark"
-make -C riscv-coremark/coremark CC=riscv64-unknown-linux-gnu-gcc compile
+set -e
+
+echo "Building Coremark Linux and Baremetal"
+cd riscv-coremark
+./build-coremark.sh
 
 echo "Moving Coremark binary to overlay"
+cd ..
 mkdir -p overlay
-mv riscv-coremark/coremark/coremark.exe overlay/coremark.riscv
+mv riscv-coremark/coremark.riscv overlay/coremark.riscv
